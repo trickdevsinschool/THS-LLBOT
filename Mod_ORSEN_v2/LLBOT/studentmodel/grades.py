9f398589554359c67eb2877367da4143e9208fd2
silverjan19 <jan_silverio@dlsu.edu.ph>
2021-05-03 19:29:36 +0800

    implemented introductory/short course of lessons

--- Mod_ORSEN_v2/LLBOT/studentmodel/grades.py
+++ Mod_ORSEN_v2/LLBOT/studentmodel/grades.py
@@ -8,7 +8,7 @@ class grades():
     def __init__(self,studentid):
        
         self.studentid= studentid #this shouldn't be an assignment, rather it should be finding a match of the studentid in the db
-        self.curr_lesson="SVA" 
+        self.curr_lesson="SVA"
         self.curr_level= "1"
         self.curr_score="0"
         self.prereq="0"
